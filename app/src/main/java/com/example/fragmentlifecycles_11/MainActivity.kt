2a6cf11e16445f49c53708fed97b5d0b9b72e108
Camarederic <camarederic@mail.ru>
2022-10-06 15:37:48 +0300

    Create Fragments

--- app/src/main/java/com/example/fragmentlifecycles_11/MainActivity.kt
+++ app/src/main/java/com/example/fragmentlifecycles_11/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.fragmentlifecycles_11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentlifecycles_11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOpen.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frameLayout, BlankFragment2.newInstance())
+                .commit()
+        }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, BlankFragment.newInstance())
+            .commit()
     }
 }
